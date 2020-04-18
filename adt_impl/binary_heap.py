16544f31259a6ef347ff2ed1a9e2d4822cfcadd2
aslisabanci <aslisabanci@gmail.com>
2020-04-17 17:13:15 -0700

    Update heap constructor to take a comparison function, to implement min and max heap

--- adt_impl/binary_heap.py
+++ adt_impl/binary_heap.py
@@ -1,11 +1,23 @@
+import operator
+
+
 class BinaryHeap:
-    def __init__(self):
+    """Binary Heap implementation of Minimum or Maximum heap data structure, using an underlying list object. 
+
+    The constructor takes a comparison function to implement a customizable heap. 
+    E.g. If the comparison function is operator.gt, the implementation becomes a Maximum Heap.
+    If the comparison function is operator.lt, the implementation becomes a Minimum Heap.
+    """
+
+    def __init__(self, comp_fn=operator.lt):
+        self.fn = comp_fn
         self.items = [0]
         self.size = 0
 
     def _move_up_item_at(self, i):
         while i // 2 > 0:
-            if self.items[i // 2] > self.items[i]:
+            if self.fn(self.items[i], self.items[i // 2]):
+                # if self.items[i // 2] > self.items[i]:
                 temp = self.items[i // 2]
                 self.items[i // 2] = self.items[i]
                 self.items[i] = temp
@@ -13,20 +25,22 @@ class BinaryHeap:
 
     def _move_down_item_at(self, i):
         while i * 2 < len(self.items):
-            min_child_idx = self._min_child_for(i)
-            print(f"min child for {self.items[i]} is {self.items[min_child_idx]}")
-            if self.items[min_child_idx] < self.items[i]:
-                temp = self.items[min_child_idx]
-                self.items[min_child_idx] = self.items[i]
+            coi_idx = self.child_of_interest_for(i)
+            print(f"min child for {self.items[i]} is {self.items[coi_idx]}")
+            # if self.items[min_child_idx] < self.items[i]:
+            if self.fn(self.items[coi_idx], self.items[i]):
+                temp = self.items[coi_idx]
+                self.items[coi_idx] = self.items[i]
                 self.items[i] = temp
-                i = min_child_idx
+                i = coi_idx
             else:
                 break
 
-    def _min_child_for(self, i):
+    def child_of_interest_for(self, i):
         if i * 2 + 1 >= len(self.items):
             return i * 2
-        return i * 2 if self.items[i * 2] < self.items[i * 2 + 1] else i * 2 + 1
+        return i * 2 if self.fn(self.items[i * 2], self.items[i * 2 + 1]) else i * 2 + 1
+        # return i * 2 if self.items[i * 2] < self.items[i * 2 + 1] else i * 2 + 1
 
     def push(self, item: object):
         self.items.append(item)
@@ -45,4 +59,4 @@ class BinaryHeap:
         self.items = [0] + items[:]
         for i in range(1, len(self.items)):
             print(f"item at i:{i} is {self.items[i]}, whole list is :{self.items}")
-            self._move_down_item_at(i)
+            self._move_up_item_at(i)
